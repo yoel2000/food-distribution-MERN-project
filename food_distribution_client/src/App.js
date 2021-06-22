e70552104e686fff11845e368a8fa224358fba6d
yoelb <yoel.bensoussan@gmail.com>
2021-06-22 18:45:21 +0300

    the first step

--- food_distribution_client/src/App.js
+++ food_distribution_client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import { render } from "react-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <BrowserRouter>
+      <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/login" component={Login} exact />
+      </Switch>
+      </BrowserRouter>
+    );
 }
 
 export default App;
