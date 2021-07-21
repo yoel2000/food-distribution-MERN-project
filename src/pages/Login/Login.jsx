7986de3e598d82fe73056d0cc8525af1c149e028
yoelb <yoel.bensoussan@gmail.com>
2021-07-21 12:35:56 +0300

    continue login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+const axios = require('axios')
+
 
 function Login() {
+
+    // yoel.bensoussan@gmail.com
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    let history= useHistory();
+
+    let mySubmitHandler = (event) => {
+        event.preventDefault()
+        axios.post("http://localhost:8080/login", {
+            'email': email,
+            'password': password
+        }).then((res)=>{console.log(res); history.push('/home')})
+
+    }
     return(
-       <h1> Hello from Login</h1>
+        <div>
+        <form onSubmit={mySubmitHandler}>
+            <input type="text" placeholder="email:" onChange={(event)=>setEmail(event.target.value)}/> <br />
+            <input type="text" placeholder="password:" onChange={(event)=>setPassword(event.target.value)}/> <br />
+            <input type="submit" value="login"/><br />
+        </form>
+        </div>
     );
 }
 
